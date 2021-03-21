9e8b18f3275f554128b97de2f1c8a04d4dbd7987
root <kchau593@gmail.com>
2021-03-21 22:08:46 +0000

    Added an updated requirements and more stuff insde the app.py so it should work

--- chat_api/app.py
+++ chat_api/app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+#sqlalchamey db
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(120))
 
 
 @app.route('/')
@@ -10,4 +20,3 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
-print('hello there')
